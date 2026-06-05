f6b22918f4b64e97a2aea0f49b2c45a267210565
jasonchee816 <jasonchee01@hotmail.com>
2026-06-05 20:09:00 +0800

    feat(struct): add images

---
+++ src/module/biometric/constants/images.ts
@@ -0,0 +1,4 @@
+export const BiometricImages = {
+  faceId: require('../../../../assets/images/face_id_icon.png'),
+  fingerprint: require('../../../../assets/images/fingerprint_icon.png'),
+} as const;
